dd75da5271f62c4834ed7d309d160d9866202c99
Charlie Vieth <charlie.vieth@gmail.com>
2015-12-09 20:44:11 -0500

    Test FilterList and FilterGo

--- fs/fs_test.go
+++ fs/fs_test.go
@@ -11,52 +11,58 @@ func TestFSInit(t *testing.T) {
 	// Make sure FS initializes itself and does not panic.
 	defer func() {
 		if e := recover(); e != nil {
-			t.Fatalf("PANIC FS Init: %+v", e)
+			t.Fatalf("FS Init Panic: %v", e)
 		}
 	}()
 
 	// Test defaults
-	var fs FS
-	fs.Readdir(".")
-	if fs.maxOpenFiles != DefaultMaxOpenFiles {
-		t.Errorf("FS Init maxOpenFiles: exp: %d got: %d", DefaultMaxOpenFiles, fs.maxOpenFiles)
-	}
-	if fs.maxOpenDirs != DefaultMaxOpenDirs {
-		t.Errorf("FS Init maxOpenDirs: exp: %d got: %d", DefaultMaxOpenDirs, fs.maxOpenDirs)
+	{
+		var fs FS
+		fs.Readdir(".")
+		if fs.maxOpenFiles != DefaultMaxOpenFiles {
+			t.Errorf("FS Init maxOpenFiles: exp: %d got: %d", DefaultMaxOpenFiles, fs.maxOpenFiles)
+		}
+		if fs.maxOpenDirs != DefaultMaxOpenDirs {
+			t.Errorf("FS Init maxOpenDirs: exp: %d got: %d", DefaultMaxOpenDirs, fs.maxOpenDirs)
+		}
 	}
 
 	// Test value
-	max := 1
-	fs = FS{
-		maxOpenFiles: max,
-		maxOpenDirs:  max,
-	}
-	fs.Readdir(".")
-	if fs.maxOpenFiles != max {
-		t.Errorf("FS Init maxOpenFiles: exp: %d got: %d", max, fs.maxOpenFiles)
-	}
-	if fs.maxOpenDirs != max {
-		t.Errorf("FS Init maxOpenDirs: exp: %d got: %d", max, fs.maxOpenDirs)
+	{
+		max := 1
+		fs := FS{
+			maxOpenFiles: max,
+			maxOpenDirs:  max,
+		}
+		fs.Readdir(".")
+		if fs.maxOpenFiles != max {
+			t.Errorf("FS Init maxOpenFiles: exp: %d got: %d", max, fs.maxOpenFiles)
+		}
+		if fs.maxOpenDirs != max {
+			t.Errorf("FS Init maxOpenDirs: exp: %d got: %d", max, fs.maxOpenDirs)
+		}
 	}
 
 	// Test -1
-	fs = FS{
-		maxOpenFiles: -1,
-		maxOpenDirs:  -1,
-	}
-	fs.Readdir(".")
-	if fs.maxOpenFiles != -1 {
-		t.Errorf("FS Init maxOpenFiles: exp: %d got: %d", -1, fs.maxOpenFiles)
-	}
-	if fs.maxOpenDirs != -1 {
-		t.Errorf("FS Init maxOpenDirs: exp: %d got: %d", -1, fs.maxOpenDirs)
-	}
-	// Make sure gates are not initialized for -1.
-	if fs.fsOpenGate != nil {
-		t.Errorf("FS Init: non-nil %s", "fsOpenGate")
-	}
-	if fs.fsDirGate != nil {
-		t.Errorf("FS Init: non-nil %s", "fsDirGate")
+	{
+		fs := FS{
+			maxOpenFiles: -1,
+			maxOpenDirs:  -1,
+		}
+		fs.Readdir(".")
+		if fs.maxOpenFiles != -1 {
+			t.Errorf("FS Init maxOpenFiles: exp: %d got: %d", -1, fs.maxOpenFiles)
+		}
+		if fs.maxOpenDirs != -1 {
+			t.Errorf("FS Init maxOpenDirs: exp: %d got: %d", -1, fs.maxOpenDirs)
+		}
+		// Make sure gates are not initialized for -1.
+		if fs.fsOpenGate != nil {
+			t.Errorf("FS Init: non-nil %s", "fsOpenGate")
+		}
+		if fs.fsDirGate != nil {
+			t.Errorf("FS Init: non-nil %s", "fsDirGate")
+		}
 	}
 }
 
@@ -65,7 +71,7 @@ var sameFileTests = []struct {
 	fi2  os.FileInfo
 	same bool
 }{
-	{ // 0: Same
+	{ // 0: Same fileStats
 		fi1: &fileStat{
 			name:    "a",
 			size:    1,
@@ -169,5 +175,28 @@ func TestSameNilFile(t *testing.T) {
 	}
 	if ok := SameFile(nil, fi); ok {
 		t.Errorf("SameFile (%v) (%v): %v", nil, nil, ok)
+	}
+}
+
+func TestFilterGo(t *testing.T) {
+	exp := []string{
+		"a.go",
+		"b.go",
+		"c.go",
+	}
+	bad := []string{
+		"A.no",
+		"B.no",
+		"C.no",
+	}
+	names := append(exp, bad...)
+	list := FilterList(names, FilterGo)
+	if len(list) != len(exp) {
+		t.Errorf("FilterGo len: Exp (%v) Got (%v)", len(exp), len(list))
+	}
+	for i := 0; i < len(exp); i++ {
+		if exp[i] != list[i] {
+			t.Errorf("FilterGo len: Exp (%v) Got (%v)", exp[i], list[i])
+		}
 	}
 }
